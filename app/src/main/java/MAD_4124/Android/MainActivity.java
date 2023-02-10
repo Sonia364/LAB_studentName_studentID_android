5d707ffd8e46d9c0e0a104ac792e0958a09b918e
Sonia364 <c0872364@mylambton.ca>
2023-02-10 18:06:05 -0500

    Initial commit

---
+++ app/src/main/java/MAD_4124/Android/MainActivity.java
@@ -0,0 +1,14 @@
+package MAD_4124.Android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
